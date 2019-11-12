24e2c857e8b8813543e7758f4e1b76f13e00ebea
arsummers <aliyasummers1@gmail.com>
2019-11-11 16:37:34 -0800

    imported controls

--- game.py
+++ game.py
@@ -1,25 +1,13 @@
 import pygame
+
+from pygame, locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
 pygame.init()
-
-# drawing window
-screen = pygame.display.set_mode([500, 500])
-
-# runs until user tells it not to
-running = True
-
-while running:
-
-    # checks if user clicked close window button
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        
-    # makes the background white
-    screen.fill((255, 255, 255))
-
-    # draws a solid blue circle in center of screen
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-
-    pygame.display.flip()
-
-pygame.quit()
