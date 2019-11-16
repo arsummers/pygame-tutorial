6cb500ac98669a09ec8a1ea52b00c765731e580c
arsummers <aliyasummers1@gmail.com>
2019-11-15 17:03:01 -0800

    added player sprite, class

--- game.py
+++ game.py
@@ -1,5 +1,19 @@
 import pygame
 import os
+import sys
+
+class Player(pygame.sprite.Sprite):
+    """
+    spawns a player
+    """
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        for i in range(1, 5):
+            img = pygame.image.load(os.path.join('images','hero' + str(i) + '.png'))
+            self.images.append(img)
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
 
 # objects
 
@@ -8,7 +22,7 @@ worldx = 800
 worldy = 600
 
 world = pygame.display.set_mode([worldx, worldy])
-backdrop = pygame.image.load(os.path.join('images', 'stage.png').convert())
+backdrop = pygame.image.load(os.path.join('images','stage.png'))
 backdropbox = world.get_rect()
 
 BLUE  = (25,25,200)
@@ -21,3 +35,23 @@ clock = pygame.time.Clock()
 pygame.init()
 
 # main game loop
+
+running = True
+
+while running == True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit(); sys.exit()
+        
+        # checks if a key is pressed
+        if event.type == pygame.KEYDOWN:
+            if event.key == ord('q'):
+                pygame.quit()
+                sys.exit()
+                running = False
+        
+    # fills world with background image
+    world.blit(backdrop, backdropbox)
+
+    pygame.display.flip()
+    clock.tick(fps)
