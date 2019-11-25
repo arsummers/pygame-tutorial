c5bbf46ffb0ba580deefa2a034dbaac1a94359fd
arsummers <aliyasummers1@gmail.com>
2019-11-25 14:34:49 -0800

    enemy not spawning

--- game.py
+++ game.py
@@ -51,6 +51,17 @@ class Player(pygame.sprite.Sprite):
                 self.frame = 0
             self.image = self.images[(self.frame//ani)+1]
 
+class Enemy(pygame.sprite.Sprite):
+    """
+    spawns an enemy
+    """
+    def __init__(self, x, y, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('images', img))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
 # objects
 
 # setup
@@ -70,6 +81,10 @@ player.rect.y = 0
 player_list = pygame.sprite.Group()
 player_list.add(player)
 steps = 10 #number of pixels to move
+
+enemy = Enemy(20, 200, 'enemy.png') #spawns enemy, 20 is x pos, 200 is y pos
+enemy_list = pygame.sprite.Group()
+enemy_list.add(enemy)
 
 
 fps = 40 #frame rate
